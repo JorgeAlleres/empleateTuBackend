c0f1711612136dbf9bf791a8e23c9800b083ff5a
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-22 13:02:19 +0100

    BBDD, Login y Register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,26 +1,27 @@
-import { AuthService } from "../services/auth.services";
-import {Response, Request} from "express";
+import { AuthService } from "@/services/auth.service";
+import {Response, Request} from 'express'
 
-export class AuthController {
-    static async register(req: Request, res: Response) {
-        try {
+export class AuthController{
+    static async register(req:Request, res:Response){
+        try{
             const userData = req.body
+            //TODO validar el body
             const newUser = await AuthService.register(userData)
-            res.status(201).json({ message: 'User register succesfully', newUser })
-        } catch (error) {
-            res.status(409).json({ message: 'User register failed' + error})
+            res.status(201).json({message:'User register successfully', newUser})
+        }catch(error){
+            res.status(409).json({message:'Fallo al registrar al usuario'+error})
         }
+
     }
-    static async login(req: Request, res: Response) {
-        try {
+    static async login(req:Request, res:Response){
+        try{
             const userData = req.body
             //TODO validar el body (opcional)
             const token = await AuthService.login(userData.email, userData.password)
-            //TODO Inyectar cookie al cliente
-            res.status(201).json({ message: 'Login succesfully', token })
-        } catch (error) {
-            res.status(409).json({ message: 'Login failed' + error })
+            //TODO inyectar cookie al cliente
+            res.status(201).json({message:'Login successfully:', token})
+        }catch(error){
+            res.status(409).json({message:'Fallo al loguearse el usuario'+error})
         }
     }
 }
-
