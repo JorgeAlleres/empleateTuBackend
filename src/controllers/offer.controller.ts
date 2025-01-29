33440891e7f852379c4bf896701aa3be9e05a4f7
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-29 12:55:30 +0100

    Creo archivos para ofertas

---
+++ src/controllers/offer.controller.ts
@@ -0,0 +1,20 @@
+export class OfferController{
+    static async getAll(req:Request, res:Response){
+        //TODO
+    }
+    static async save(req:Request, res:Response){
+        //TODO
+    }
+    static async delete(req:Request, res:Response){
+        //TODO
+    }
+    static async update(req:Request, res:Response){
+        //TODO
+    }
+    static async rate(req:Request, res:Response){
+        //TODO
+    }
+    static async getRate(req:Request, res:Response){
+        //TODO
+    }
+}
---
+++ src/routes/offer.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {OfferController} from '../controllers/offer.controller'
+
+const router = Router()
+
+//Listar todas las ofertas localhost:3000/api/offerts/?title=react&category=DAM
+router.get('/', OfferController.getAll)
+//Añadir una oferta nueva POST localhost:3000/api/offerts/{body}
+router.post('/', OfferController.save)
+// DELETE Borrar una oferta localhost:3000/api/offerts/XXXX
+router.delete('/:id', OfferController.delete)
+// MODIFICAR Actualizar una oferta localhost:3000/api/offerts/XXXX  {body}
+router.put('/id', OfferController.update)
+
+//Calificamos una oferta    {body}
+router.post('/:id/rate', OfferController.rate)
+// Vemos que calificaion total se le ha dado a una oferta
+router.get('/:id/rate/', OfferController.getRate)
+
+export default router
