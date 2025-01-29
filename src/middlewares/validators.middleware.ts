2c0bad8bba12024f13310cd173cc7a14682c4d13
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-29 11:42:22 +0100

    Validacion del registro

---
+++ src/middlewares/validators.middleware.ts
@@ -0,0 +1,8 @@
+import { body } from "express-validator"
+
+export const registerValidation = [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:4}).withMessage('Password too short'),
+    body('name').notEmpty().withMessage('Name required'),
+    
+]
