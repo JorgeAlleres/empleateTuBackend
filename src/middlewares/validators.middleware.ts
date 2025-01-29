face9929321d2d9651c7d3b60d8947062588cafb
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-29 11:47:41 +0100

    Validacion Login

--- src/middlewares/validators.middleware.ts
+++ src/middlewares/validators.middleware.ts
@@ -4,5 +4,11 @@ export const registerValidation = [
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({min:4}).withMessage('Password too short'),
     body('name').notEmpty().withMessage('Name required'),
+
+]
+
+export const loginValidation = [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:4}).withMessage('Password too short')
     
 ]
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import {AuthController} from '../controllers/auth.controller'
-import { registerValidation } from "@/middlewares/validators.middleware";
+import { loginValidation, registerValidation } from "@/middlewares/validators.middleware";
 import { ValidationMiddleware } from "@/middlewares/validation.middleware";
 
 const router = Router()
 
-router.post('/login', AuthController.login)
+router.post('/login',loginValidation, ValidationMiddleware, AuthController.login)
 //router.post('/login', AuthController.logout)
 router.post('/register',registerValidation, ValidationMiddleware, AuthController.register)
 
