1faa20bb201f192917d48b49f4a83af1a6b49a2f
JorgeAlleres <alleresjorge10@gmail.com>
2025-01-31 08:54:13 +0100

    Primer deploy

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(cookieParser())
 //Cambiar la url cuando deploy
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
