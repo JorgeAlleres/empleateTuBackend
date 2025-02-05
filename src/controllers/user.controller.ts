0c7615f0674d44839971405f36c3084689171b71
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-05 12:21:34 +0100

    corrijo errores usercontroller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import {Response, Request, NextFunction} from 'express'
 export class UserController{
     static async profile(req:Request, res:Response, next:NextFunction) {
         try {
-            const email = req.body.user.email
+            const email = req.user.email
             const user =  await UserService.getByEmail(email)
             res.status(200).json(user)
         } catch(error) {
