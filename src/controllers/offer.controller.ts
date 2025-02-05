64e91fa06b43887cfc7923e09bfc37e3b951a157
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-05 12:21:12 +0100

    Corrijo errores ofercontroller

--- src/controllers/offer.controller.ts
+++ src/controllers/offer.controller.ts
@@ -3,6 +3,19 @@ import { OfferService } from "../services/offer.service"
 import { NextFunction, Request, Response } from "express"
 
 export class OfferController {
+    static async getById(req:Request, res:Response, next:NextFunction){
+        try{
+            const id = Number.parseInt(req.params.id)
+            if (isNaN(id)) throw new HttpException(400, "Invalid offer ID");
+
+            // pasar a entero
+            const offer = await OfferService.getById(id)
+            res.status(200).json(offer)
+        }catch(error){
+            next(error)
+        }
+    }
+
     static async getAll(req: Request, res: Response, next: NextFunction) {
         try {
             const offers = await OfferService.getAll()
@@ -14,7 +27,9 @@ export class OfferController {
     static async create(req: Request, res: Response, next:NextFunction) {
         try {
             const offersData = req.body
-            const newOffer = await OfferService.create(offersData)
+            const userId = req.user?.id
+            if (!userId) throw new HttpException(400, "User creator ID is required");
+            const newOffer = await OfferService.create(offersData, userId)
             res.status(201).json({ message: 'Offer register successfully', newOffer })
         } catch (error) {
             next(error)
@@ -22,7 +37,10 @@ export class OfferController {
     }
     static async delete(idOffer:number, req:Request, res:Response, next:NextFunction) {
         try {
-            const offerDeleted = await OfferService.delete(idOffer)
+            const id = Number.parseInt(req.params.id)
+            if (isNaN(id)) throw new HttpException(400, "Invalid offer ID");
+
+            const offerDeleted = await OfferService.delete(id)
             res.status(201).json({ message: 'Offer deleted successfully', offerDeleted })
         } catch (error) {
             next(error)
@@ -30,8 +48,11 @@ export class OfferController {
     }
     static async update(id:number, req: Request, res: Response, next:NextFunction) {
         try {
-            const offersData = req.body
-            const updatedOffer = await OfferService.update(id, offersData)
+            const offerData = req.body
+            const id = Number.parseInt(req.params.id)
+            if (isNaN(id)) throw new HttpException(400, "Invalid offer ID");
+
+            const updatedOffer = await OfferService.update(id, offerData)
             res.status(201).json({ message: 'Offer updated successfully', updatedOffer })
         } catch (error) {
             next(error)
@@ -39,13 +60,13 @@ export class OfferController {
     }
     static async rate(req:Request, res:Response, next: NextFunction){
         try{
-            const id = Number.parseInt(req.params.id)
-            if (isNaN(id)) throw new HttpException(400, "Invalid offer ID");
+            const offerId = Number.parseInt(req.params.id)
+            if (isNaN(offerId)) throw new HttpException(400, "Invalid offer ID");
 
             const {value} = req.body
-            const userId = req.body.user.id
+            const userId = req.user.id
 
-            await OfferService.rate(userId, id, value)
+            await OfferService.rate(userId, offerId, value)
             res.status(200).json({message: 'Offer rate successfully'})
         }catch(error){
             next(error)
@@ -54,10 +75,10 @@ export class OfferController {
 
     static async getRate(req:Request, res:Response, next: NextFunction){
         try{
-            const id = Number.parseInt(req.params.id)
-            if (isNaN(id)) throw new HttpException(400, "Invalid offer ID");
+            const offerId = Number.parseInt(req.params.id)
+            if (isNaN(offerId)) throw new HttpException(400, "Invalid offer ID");
 
-            await OfferService.getRate(id)
+            await OfferService.getRate(offerId)
             res.status(200).json({message: 'Offer rate successfully'})
         }catch(error){
             next(error)
