523950d5b4d4185a2553bd02beb4f7f27f81daac
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-10 13:06:31 +0100

    Añado ruta cors para deploy

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(cookieParser())
 //Cambiar la url cuando deploy
 
 app.use(cors({
-    origin: ['http://localhost:5173','*'],
+    origin: ['http://localhost:5173','https://empleatetubackend-vl3k.onrender.com' ,'*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
